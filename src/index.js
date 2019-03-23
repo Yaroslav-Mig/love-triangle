450edf713013d2b41bd17a74ccc9b3e3f9ad2bf9
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2019-03-23 18:13:44 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  const length = preferences.length;
+  for (let i = 0; i < length; i++) {
+    const firstValue = preferences[i];
+    const secondValue = preferences[firstValue - 1];
+    const thirdValue = preferences[secondValue - 1];
+
+    const firstCondition = i + 1 !== firstValue;
+    const secondCondition = i + 1 === thirdValue;
+
+    if (firstCondition && secondCondition) {
+      count++;
+    }
+  }
+  return count / 3;
 };
